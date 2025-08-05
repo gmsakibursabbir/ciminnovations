0bfcca1a943ff684a618398ce4a510898908c9b4
DESKTOP-3P213DA\sakibur <gmsakibursabbir@gmail.com>
2025-08-05 10:49:45 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const images = [
+  {
+    url: "../assets/img/hero-slider-01.png",
+    heading: "Share, connect, <span>grow</span>",
+    subtext: "Spread the word through your network of friends and family",
+  },
+  {
+    url: "../assets/img/hero-slider-02.png",
+    heading: "Create Wholesome <span>content</span>",
+    subtext: "Your voice has the power to make an impact",
+  },
+  {
+    url: "../assets/img/hero-slider-03.png",
+    heading: "Boost your events and reach more in a <span>click</span>",
+    subtext: "Stream live prayer meetings, devotional services, and small‑group fellowships with zero tech headache.",
+  },
+];
+
+let index = 0;
+
+function updateHero() {
+  const hero = document.getElementById("landing-hero");
+  const heading = document.getElementById("hero-heading");
+  const subtext = document.getElementById("hero-subtext");
+
+  // Fade out
+  heading.style.opacity = "0";
+  subtext.style.opacity = "0";
+
+  setTimeout(() => {
+    // Change content
+    index = (index + 1) % images.length;
+    hero.style.backgroundImage = `url(${images[index].url})`;
+    heading.innerHTML = images[index].heading;
+    subtext.textContent = images[index].subtext;
+
+    // Fade in
+    heading.style.opacity = "1";
+    subtext.style.opacity = "1";
+  }, 1000);
+}
+
+setInterval(updateHero, 5000);
